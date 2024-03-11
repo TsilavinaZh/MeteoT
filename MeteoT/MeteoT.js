d9ba41100c9a12ad8fde332589b05953eb5e1c4a
Tsilavina <159104790+TsilavinaZh@users.noreply.github.com>
2024-03-11 22:08:28 +0300

    Update MeteoT.js

--- MeteoT/MeteoT.js
+++ MeteoT/MeteoT.js
@@ -4,14 +4,19 @@ require('dotenv').config();
 const API_KEY = process.env.API_KEY;
 
 const getWeather = async (city) => {
+  if (!city || typeof city !== 'string') {
+    throw new Error('Invalid or missing city name');
+  }
+
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
   try {
-    const response = await axios.get(url);
-    const { data } = response;
+    const { data } = await axios.get(url);
     return data;
   } catch (error) {
-    throw error;
+    // Vous pouvez personnaliser la gestion des erreurs en fonction du status code de la réponse.
+    console.error('Error fetching weather data:', error.message);
+    throw error; // Renvoie l'erreur pour une éventuelle gestion à un niveau supérieur.
   }
 };
 
